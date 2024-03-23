3343edaf21f199aa600c8dbeebe3ab2538db09ff
Tianyi Yin <tianyiy.ubc@gmail.com>
2024-03-23 14:48:18 -0700

    connected the database to our project

--- src/Config/SupabaseClient.js
+++ src/Config/SupabaseClient.js
@@ -1,6 +1,8 @@
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
-const supabaseKey = process.env.REACT_APP_ANON_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+import { createClient } from "@supabase/supabase-js";
 
-export default supabase
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_ANON_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,41 @@
-import supabase from "../Config/SupabaseClient"
+import supabase from "../config/supabaseClient";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [fetchError, setFetchError] = useState(null);
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const { data, error } = await supabase.from("studyBuddies").select();
+      if (error) {
+        setFetchError("Could not fetch the posts");
+        setPosts(null);
+        console.log(error);
+      }
+      if (data) {
+        setPosts(data);
+        setFetchError(null);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  console.log(supabase);
   return (
     <div className="page home">
       <h2>Home</h2>
+      {fetchError && <p>{fetchError}</p>}
+      {posts && (
+        <div className="posts">
+          {posts.map((post) => (
+            <p>{post.postTitle}</p>
+          ))}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
