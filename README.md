307ec3c141beba6b5af3fde9845b4c8b5da0da01
Tianyi Yin <tianyiy.ubc@gmail.com>
2024-03-23 12:52:20 -0700

    first attempt to commit

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# Hacks2024
-Project for hackathon (UBC BCS) - 2024
-    Goal: to build a website where you can find study companions by searching a course code, and you will need to add your contact information, couses taking, available times etc 
+# Starter React Project for the Supabase Course
+
+**How to run the starter project:**
+
+1. Once downloaded, navigate to the project directory in a terminal and run **npm install** to install all the project dependencies.
+2. In the project directory, run **npm start** to run the app & view it in a browser at [http://localhost:3000](http://localhost:3000).
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+// pages
+import Home from "./pages/Home";
+import Create from "./pages/Create";
+import Update from "./pages/Update";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <h1>Study Buddies</h1>
+        <div>
+          <Link to="/">Search</Link>
+        </div>
+        <div>
+          <Link to="/create">Create A New Post to find a study partner</Link>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/:id" element={<Update />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
