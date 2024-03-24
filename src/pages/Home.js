b515251d05418597074320c9293856ddc90bea81
Phillip <hfqiao@students.cs.ubc.ca>
2024-03-23 21:44:09 -0700

    added in Search bar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,8 +13,22 @@ const Home = () => {
   const [fetchError, setFetchError] = useState(null);
   const [posts, setPosts] = useState(null);
   const [orderBy, setOrderBy] = useState('created_at')
-  //const posts = [{ title: "hello" }, { title: "world" }];
+  const [searchDpt, setSearchDpt] = useState("");
+  const [filteredPosts, setFilteredPosts] = useState(null);
 
+  const handleSearchDpt = (event) => {
+    const query = event.target.value;
+    setSearchDpt(query);
+    if (query === "") {
+      setFilteredPosts(null);
+      return;
+    }
+
+    const filtered = posts.filter((post) =>
+      post.courseDpt.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredPosts(filtered);
+  };
 
   const handleDelete = (id) => {
     setPosts(prevPosts => {
@@ -48,8 +62,15 @@ const Home = () => {
   return (
     <div className="page home">
       <h2>Active Posts</h2>
+      <input
+        type="text"
+        placeholder="Search Courses..."
+        value={searchDpt}
+        onChange={handleSearchDpt}
+      />
+      
       {fetchError && <p>{fetchError}</p>}
-      {posts && (
+      {filteredPosts && (
         <div className="posts">
           <div className="order-by">
             <p>Order by:</p>
@@ -57,13 +78,26 @@ const Home = () => {
             <button onClick={() => setOrderBy('courseDpt')}>Subject </button>
 
           </div>
-          <div className = "post-grid">
-            {posts.map(post => (
-              // <p>{post.postTitle}</p>
-              <PostCard 
-              key={post.id} 
-              post={post} 
-              onDelete={handleDelete}/>
+          <div className="post-grid">
+            {filteredPosts.map((post) => (
+              // <p>{post.postTitle}</p >
+              <PostCard key={post.id} post={post} setPosts={setPosts} />
+            ))}
+          </div>
+        </div>
+      )}
+      {!filteredPosts && posts && (
+        <div className="posts">
+          <div className="order-by">
+            <p>Order by:</p>
+            <button onClick={() => setOrderBy('created_at')}>Time Created</button>
+            <button onClick={() => setOrderBy('courseDpt')}>Subject </button>
+
+          </div>
+          <div className="post-grid">
+            {posts.map((post) => (
+              // <p>{post.postTitle}</p >
+              <PostCard key={post.id} post={post} setPosts={setPosts} />
             ))}
           </div>
         </div>
